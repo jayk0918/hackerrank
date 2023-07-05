25c93b52303d1496eead4438159d50eb93ff2997
jayk0918 <jesper2able@gmail.com>
2023-07-05 22:28:17 +0900

    23.07.05

---
+++ src/com/jayk0918/www/DiagonalDifference.java
@@ -0,0 +1,11 @@
+package com.jayk0918.www;
+
+import java.io.IOException;
+
+public class DiagonalDifference {
+    public static void main(String[] args) throws IOException {
+    	
+    	
+    	
+    	
+}
--- src/com/jayk0918/www/PlusMinus.java
+++ src/com/jayk0918/www/PlusMinus.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.stream.Stream;
+
+
 class Result {
 
     /*
